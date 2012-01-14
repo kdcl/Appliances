a4010f62d75e03f28d8de8614a6c6b994675aba2
kdcl <kdcl03@gmail.com>
2012-01-14 14:25:52 +0800

    try

---
+++ Appliances/Appliance.h
@@ -0,0 +1,21 @@
+//
+//  Appliance.h
+//  Appliances
+//
+//  Created by  on 12/1/14.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Appliance : NSObject
+{
+    NSString *productName;
+    int voltage;
+    
+}
+
+@property (copy) NSString *productName;
+@property int voltage;
+
+@end
---
+++ Appliances/Appliance.m
@@ -0,0 +1,23 @@
+//
+//  Appliance.m
+//  Appliances
+//
+//  Created by  on 12/1/14.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Appliance.h"
+
+@implementation Appliance
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
